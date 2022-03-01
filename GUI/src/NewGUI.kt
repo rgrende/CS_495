2465e160c8d585191bef0996cc22ddb0e5fb97eb
rgrende <71292508+rgrende@users.noreply.github.com>
2022-03-01 09:30:56 -0700

    Delete GUI/src directory

--- GUI/src/NewGUI.kt
+++
@@ -1,8 +0,0 @@
-import java.awt.Image
-
-class NewGUI {
-    var imageIcon: ImageIcon = ImageIcon("./img/imageName.png") // load the image to a imageIcon
-    var image: Image = imageIcon.getImage() // transform it
-    var newimg: Image = image.getScaledInstance(120, 120, Image.SCALE_SMOOTH) // scale it the smooth way
-    fun ImageIcon() // transform it back
-}
